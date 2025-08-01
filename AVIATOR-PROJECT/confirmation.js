93d055b633e844f7c2e6470dc00fcba364ca3aff
KLINS-254 <klinswoo@gmail.com>
2025-08-01 22:15:26 +0300

    Delete AVIATOR-PROJECT/confirmation.js

--- AVIATOR-PROJECT/confirmation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const price = urlParams.get('price');
-    
-    if (price) {
-        document.getElementById('payment-amount-display').textContent = price;
-    }
-});
